59f755fef34ec797cdf656c6c8859b249a18c375
Praveen <16594062+pravsterrr@users.noreply.github.com>
2021-05-25 01:29:03 +0100

    Added navbar

--- lib/main.dart
+++ lib/main.dart
@@ -5,37 +5,47 @@
 import 'package:flutter/material.dart';
 import 'package:floating_bottom_navigation_bar/floating_bottom_navigation_bar.dart';
 
-void main() => runApp(MaterialApp(
-      home: Home(),
-    ));
+void main() => runApp(MyApp());
 
-class Home extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _index = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('The Help App'),
-        centerTitle: true,
-        backgroundColor: Colors.red[600],
-      ),
-      body: Center(
-          child: ElevatedButton(
-        onPressed: () {
-          print('youclickedme');
-        },
-        child: Text('hello'),
-      )),
-      bottomNavigationBar: FloatingNavbar(
-        onTap: (int val) {
-          print(val);
-        },
-        currentIndex: 1,
-        items: [
-          FloatingNavbarItem(icon: Icons.map_rounded, title: 'Map'),
-          FloatingNavbarItem(
-              icon: Icons.health_and_safety_rounded, title: 'Home'),
-          FloatingNavbarItem(icon: Icons.favorite_rounded, title: 'CPR'),
-        ],
+    return MaterialApp(
+      title: 'Help',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Help'),
+          backgroundColor: Colors.red[300],
+          centerTitle: true,
+        ),
+        //If you want to show body behind the navbar, it should be true
+        extendBody: true,
+        body: Center(
+          child: Text(
+            "This is Tab $_index.",
+            style: TextStyle(
+              fontSize: 52,
+            ),
+          ),
+        ),
+        bottomNavigationBar: FloatingNavbar(
+          onTap: (int val) => setState(() => _index = val),
+          currentIndex: _index,
+          items: [
+            FloatingNavbarItem(icon: Icons.map_rounded, title: 'Map'),
+            FloatingNavbarItem(
+                icon: Icons.health_and_safety_rounded, title: 'Home'),
+            FloatingNavbarItem(icon: Icons.favorite_rounded, title: 'CPR'),
+          ],
+        ),
       ),
     );
   }
