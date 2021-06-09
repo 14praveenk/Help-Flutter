faae6e12ff5299b14e14dfdb29395e62545fa876
Praveen Kathirvasan <16594062+pravsterrr@users.noreply.github.com>
2021-06-09 23:50:28 +0100

    .env=>dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ import 'package:georange/georange.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
-  await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: "dotenv");
   WidgetsFlutterBinding.ensureInitialized();
   runApp(App());
 }
